a658c79a5f31d3d16af1b80b1bd98b4219a7d831
Ionut <ionut.leustean@outlook.com>
2017-05-30 16:12:58 +0300

    autoComplete

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -12,4 +12,48 @@ export class FormsComponent implements OnInit {
   ngOnInit() {
   }
 
+  //autocomplete
+
+  text: string;
+  multipleText: string;
+
+  results: string[];
+  multipleResults: string[];
+
+  rawResults: string[] = ['Gigi','Vasile','Marian','Gogu','Guta'];
+
+  search(event) {
+    this.results = [];
+
+    if(event.query) {
+      this.results = this.rawResults.filter((value) => {
+        return value.toLowerCase().indexOf(event.query.toLowerCase()) !== -1;
+      });
+    }
+    else {
+      this.results = this.rawResults;
+    }
+  }
+
+  handleDropdown(event) {
+    this.results = [];
+
+    setTimeout(() => {
+      this.search(event);
+    }, 100)
+
+  }
+  searchMultiple(event) {
+    this.multipleResults = [];
+
+    if(event.query) {
+      this.multipleResults = this.rawResults.filter((value) => {
+        return value.toLowerCase().indexOf(event.query.toLowerCase()) !== -1;
+      });
+    }
+    else {
+      this.multipleResults = this.rawResults;
+    }
+  }
+
 }
