ed5ab4c045a572f9d42ec2a7c31c0792da7d6a1c
Ionut <ionut.leustean@outlook.com>
2017-05-30 15:20:04 +0300

    prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+
+// ng modules
+import {AutoCompleteModule, InputTextareaModule, InputTextModule} from 'primeng/primeng';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InputTextModule,
+    InputTextareaModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
